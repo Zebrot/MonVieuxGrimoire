d71f5550eaf4798654bcad6054a9ad30f3de0557
Pierre Terrancle <pi.terrancle@gmail.com>
2025-04-28 16:33:41 +0200

    in case of no '/images', mkdir

--- middleware/sharp-config.js
+++ middleware/sharp-config.js
@@ -1,12 +1,20 @@
 const sharp = require('sharp');
 const path = require ('path');
+const fs = require('fs');
 
 module.exports = async (req, res, next) => {
     const { buffer, originalname } = req.file;
     const timestamp = Date.now();
     const name = originalname.replace(/[^a-zA-Z0-9-_\.]/g, '_');
     const ref = `${timestamp}-${name}.webp`;
-    const filepath = path.join(__dirname, '..', 'images', ref);
+    const dirPath = path.join(__dirname, '..', 'images');
+    const filepath = path.join(dirPath, ref);
+
+    fs.access(dirPath, (error) => {
+        if(error)
+            fs.mkdirSync(dirPath);
+    })
+    
     try {
         await sharp(buffer)
             .webp({quality : 20})
